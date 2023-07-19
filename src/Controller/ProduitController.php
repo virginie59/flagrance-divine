d61c224285b418b7c2fb25810269186765ed58da
virginie lebleu <virginie.lebleu59@gmail.com>
2023-07-19 22:33:26 +0200

    page parfum homme

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -7,13 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/', name: 'app_produit')]
+#[Route('/', name: 'produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/', name: 'app_ParfumF')]
+    #[Route('/', name: 'app_produit_ParfumF')]
     public function index(ProduitRepository $produitRepository): Response
     {
         return $this->render('produit/ParfumF.html.twig', [ 
+            'ProduitRepository'=>$produitRepository
+        ]);
+    }
+
+    #[Route('/', name: 'app_ParfumH')]
+    public function edit(ProduitRepository $produitRepository): Response
+    {
+        return $this->render('produit/ParfumH.html.twig', [ 
             'ProduitRepository'=>$produitRepository
         ]);
     }
