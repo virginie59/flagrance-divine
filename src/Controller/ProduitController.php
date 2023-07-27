c5b8dc90a2025c4dd1befbb7a0e361bf56a5b68e
virginie lebleu <virginie.lebleu59@gmail.com>
2023-07-27 14:35:21 +0200

    template produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -12,37 +12,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProduitController extends AbstractController
 {
     #[Route('/ParfumF', name: 'app_produit_ParfumF')]
-    public function index(ProduitRepository $produitRepository, Produit $produit): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
         $produits = $produitRepository->findAll();
 
         $user = $this->getUser();
 
         return $this->render('produit/ParfumF.html.twig', [ 
-            'ProduitRepository'=>$produitRepository
+            'ProduitRepository'=>$produitRepository,
+            'produits' => $produits,
+            'user' => $user
         ]);
     }
 
    #[Route('/ParfumH', name: 'app_produit_ParfumH')]
-    public function edit(ProduitRepository $produitRepository, Produit $produit): Response
+    public function edit(ProduitRepository $produitRepository ): Response
     {
         $produits = $produitRepository->findAll();
 
         $user = $this->getUser();
 
         return $this->render('produit/ParfumH.html.twig', [ 
-            'ProduitRepository'=>$produitRepository
+            'ProduitRepository'=>$produitRepository,
+            'produits' => $produits,
+            'user' => $user
         ]);
     }
     #[Route('/CollectionSoins', name: 'app_produit_CollectionSoins')]
-    public function show(ProduitRepository $produitRepository, produit $produit): Response
+    public function show(ProduitRepository $produitRepository): Response
     {
         $produits = $produitRepository->findAll();
 
         $user = $this->getUser();
 
-        return $this->render('produit/ParfumH.html.twig', [ 
-            'ProduitRepository'=>$produitRepository
+        return $this->render('produit/CollectionSoins.html.twig', [ 
+            'ProduitRepository'=>$produitRepository,
+            'produits' => $produits,
+            'user' => $user
         ]);
     }
 }
